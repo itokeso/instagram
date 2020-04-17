14ee0508656f563b8e072eacb005ab824557d242
itokeso <61177741+itokeso@users.noreply.github.com>
2020-04-17 18:10:11 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,10 +9,12 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.assets.initialize_on_precompile = false
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
